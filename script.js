2ea6ba29888363ca467b8fef79a13ad2c63fd397
Danish Suhail <134198476+Danish-Suhail@users.noreply.github.com>
2026-06-30 16:13:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
-} 
+	event.preventDefault();
+	let radius = document.getElementById('radius');
+	let r = parseFloat(radius.value);
+
+	if(isNaN(r) || r < 0){
+		alert(NaN);
+		return;
+	}
+
+	let V = (4/3) * Math.PI * Math.pow(r,3);
+	let vol = document.getElementById('volume');
+	vol.value = V.toFixed(4);
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
